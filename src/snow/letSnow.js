53d51ea3f5ed4f9dc23cb65f9032e9e2158761f0
Shelenium <elena.shokhan@gmail.com>
2018-12-28 00:24:52 +0300

    Style correction

--- src/snow/letSnow.js
+++ src/snow/letSnow.js
@@ -6,39 +6,38 @@ export default class LetSnow {
     constructor() {
         this.toggleSnow = this.toggleSnow.bind(this);
         this.init = this.init.bind(this);
+        this.resizeCanvasElements = this.resizeCanvasElements.bind(this);
         this.snowAnimation = null;
         this.snowFlakes = null;
         this.snowflakesContext = null;
+        this.snowflakesCanvas = null;
+        this.aaSnowConfig = { snowflakes: '200' }; 
     }
 
     init() {
-        const resizeCanvasElements = () => {
-            snowflakesCanvas.width = window.innerWidth;
-            snowflakesCanvas.height = window.innerHeight;
-        };
-        const onDOMLoaded = () => {
-            this.snowFlakes.generate(aaSnowConfig.snowflakes);
-            this.snowAnimation.addFrameRenderer(this.snowFlakes.renderFrame, this.snowflakesContext);
-            window.addEventListener('resize', resizeCanvasElements);
-        }
-        const aaSnowConfig = { snowflakes: '200' }; 
-        let snowflakesCanvas = null;
         this.snowAnimation = new SnowAnimation();
         this.snowFlakes = new SnowFlakes();
         const element = document.createElement('canvas');
         element.id = 'snowflakesCanvas';
         document.querySelector('.snow__container').appendChild(element);
     
-        snowflakesCanvas = document.getElementById('snowflakesCanvas');
-        resizeCanvasElements();
+        this.snowflakesCanvas = document.getElementById('snowflakesCanvas');
         this.snowflakesContext = snowflakesCanvas.getContext('2d');
-
-        document.addEventListener('DOMContentLoaded', onDOMLoaded);
+        this.snowFlakes.generate(this.aaSnowConfig.snowflakes);
+        this.snowAnimation.addFrameRenderer(this.snowFlakes.renderFrame, this.snowflakesContext);
     }
 
+    resizeCanvasElements() {
+        this.snowflakesCanvas.width = window.innerWidth;
+        this.snowflakesCanvas.height = window.innerHeight;
+        this.snowFlakes.updateBounds();
+    };
+
     toggleSnow() {
+        this.resizeCanvasElements();
         this.snowFlakes.clearFrame(this.snowflakesContext);
-        this.snowAnimation.toggle();
+        this.snowAnimation.toggle(this.resizeCanvasElements);
+
     };
 
 }
